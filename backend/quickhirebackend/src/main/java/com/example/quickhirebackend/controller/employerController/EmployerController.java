496ae74dc9cc26350531ae3bc8a8bc7d97dc4e07
Ramaruva <ramaruva54@gmail.com>
2024-04-02 14:16:03 -0500

    intial code for professional controller and employercontroller

---
+++ backend/quickhirebackend/src/main/java/com/example/quickhirebackend/controller/employerController/EmployerController.java
@@ -0,0 +1,14 @@
+package com.example.quickhirebackend.controller.employerController;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployerController {
+
+
+    //need to decide the apis
+    @PostMapping("/")
+    public void employerRegister(){}
+
+}
