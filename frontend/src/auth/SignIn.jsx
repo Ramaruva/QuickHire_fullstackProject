5a12a3411855f3e70374988ba0ab5bafe6fc5b0a
Gowtham Dongari <gowthamdongari@gmail.com>
2024-04-24 10:56:23 -0500

    forgot password issues resolved

--- frontend/src/auth/SignIn.jsx
+++ frontend/src/auth/SignIn.jsx
@@ -205,7 +205,7 @@ const SignIn = () => {
               <p className="mt-10 text-center text-sm text-gray-500">
                 Not a member?{" "}
                 <Link
-                  to="/SignUp"
+                  to="/"
                   className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                 >
                   Create an Account
